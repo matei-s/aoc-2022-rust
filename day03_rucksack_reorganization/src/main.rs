9b40c7d493da895fcf5bb5f7a7d2dbd325eef3d5
Matei Simtinică <matei.simtinica@gmail.com>
2024-01-07 19:13:24 +0200

    day 3, part 2

--- day03_rucksack_reorganization/src/main.rs
+++ day03_rucksack_reorganization/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let lines: Vec<String> = stdin.lock().lines().filter_map(Result::ok).collect();
 
     part_1(&lines);
+    part_2(&lines);
 }
 
 fn part_1(lines: &Vec<String>) {
@@ -17,12 +18,29 @@ fn part_1(lines: &Vec<String>) {
         for c in comp1.intersection(&comp2) {
             let priority = priority_of(*c as u8);
 
-            println!("{:?} {priority}", comp1.intersection(&comp2));
             priorities += priority;
         }
     }
 
     println!("part 1: {priorities}")
+}
+
+fn part_2(lines: &Vec<String>) {
+    let mut priorities = 0;
+    for line in lines.chunks(3) {
+        let elf1: HashSet<char> = line[0].chars().collect();
+        let elf2: HashSet<char> = line[1].chars().collect();
+        let elf3: HashSet<char> = line[2].chars().collect();
+
+        let first_two = elf1.intersection(&elf2).cloned().collect::<HashSet<char>>();
+
+        for c in first_two.intersection(&elf3) {
+            let priority = priority_of(*c as u8);
+            priorities += priority;
+        }
+    }
+
+    println!("part 2: {priorities}");
 }
 
 fn priority_of(c: u8) -> u32 {
