ed05d77835b611d2cd62e23bb4b844f265ecaae4
Matei Simtinică <matei.simtinica@gmail.com>
2024-01-08 12:57:25 +0200

    day 8, part 2

--- day08_treetop_tree_house/src/main.rs
+++ day08_treetop_tree_house/src/main.rs
@@ -1,54 +1,171 @@
-use std::io::stdin;
+use std::{
+    fmt::{Debug, Display},
+    io::stdin,
+    ops::{Index, IndexMut},
+};
 
-fn main() {
-    let lines = stdin().lines().filter_map(Result::ok);
-
-    let mut grid: Vec<u8> = Vec::new();
-
-    let mut w = 0usize;
-    let mut h = 0usize;
-
-    for line in lines {
-        let mut line = Vec::from(line);
-        w = line.len();
-        h += 1;
-
-        grid.append(&mut line);
-    }
-
-    println!("part 1: {}", compute_visible(&grid, w, h));
+struct Grid<T> {
+    v: Vec<T>,
+    w: usize,
+    h: usize,
 }
 
-fn compute_visible(grid: &Vec<u8>, w: usize, h: usize) -> usize {
-    let mut visible = vec![false; grid.len()];
+impl Grid<u8> {
+    fn parse(lines: &Vec<String>) -> Grid<u8> {
+        let mut v: Vec<u8> = Vec::new();
+        let mut w = 0usize;
+        let mut h = 0usize;
+
+        for line in lines {
+            let mut line = line.as_bytes().to_vec();
+            w = line.len();
+            h += 1;
+
+            v.append(&mut line);
+        }
+
+        for i in 0..v.len() {
+            v[i] -= b'0';
+        }
+
+        Grid { v, w, h }
+    }
+}
+
+impl<T: Default + Clone + Display + Debug> Grid<T> {
+    fn new(w: usize, h: usize) -> Grid<T> {
+        Grid {
+            v: vec![T::default(); w * h],
+            w,
+            h,
+        }
+    }
+
+    #[allow(dead_code)]
+    fn print(&self) {
+        for i in 0..self.h {
+            for j in 0..self.w {
+                print!("{:?} ", self[(i, j)]);
+            }
+            println!();
+        }
+    }
+}
+
+impl<T> Index<(usize, usize)> for Grid<T> {
+    type Output = T;
+
+    fn index(&self, (i, j): (usize, usize)) -> &T {
+        &self.v[i * self.w + j]
+    }
+}
+
+impl<T> IndexMut<(usize, usize)> for Grid<T> {
+    fn index_mut(&mut self, (i, j): (usize, usize)) -> &mut T {
+        &mut self.v[i * self.w + j]
+    }
+}
+
+fn main() {
+    let lines: Vec<String> = stdin().lines().filter_map(Result::ok).collect();
+
+    let grid = Grid::parse(&lines);
+
+    println!("part 1: {}", compute_visible(&grid));
+    println!("part 2: {}", compute_scenic_score(&grid));
+}
+
+fn compute_scenic_score(grid: &Grid<u8>) -> usize {
+    let mut up: Grid<usize> = Grid::new(grid.w, grid.h);
+    let mut down: Grid<usize> = Grid::new(grid.w, grid.h);
+    let mut left: Grid<usize> = Grid::new(grid.w, grid.h);
+    let mut right: Grid<usize> = Grid::new(grid.w, grid.h);
+
+    // viewing distance up-down
+    for j in 0..grid.w {
+        let mut last_stop = vec![0usize; 10];
+        for i in 0..grid.h {
+            let stop = last_stop[grid[(i, j)] as usize..].iter().max().unwrap();
+            up[(i, j)] = *stop;
+            last_stop[grid[(i, j)] as usize] = i;
+        }
+
+        last_stop = vec![grid.h - 1; 10];
+        for i in (0..grid.h).rev() {
+            let stop = last_stop[grid[(i, j)] as usize..].iter().min().unwrap();
+            down[(i, j)] = *stop;
+            last_stop[grid[(i, j)] as usize] = i;
+        }
+    }
+
+    // viewing distance left-right
+    for i in 0..grid.h {
+        let mut last_stop = vec![0usize; 10];
+
+        for j in 0..grid.w {
+            let stop = last_stop[grid[(i, j)] as usize..].iter().max().unwrap();
+            left[(i, j)] = *stop;
+            last_stop[grid[(i, j)] as usize] = j;
+        }
+
+        last_stop = vec![grid.w - 1; 10];
+        for j in (0..grid.w).rev() {
+            let stop = last_stop[grid[(i, j)] as usize..].iter().min().unwrap();
+            right[(i, j)] = *stop;
+            last_stop[grid[(i, j)] as usize] = j;
+        }
+    }
+
+    let mut max = 0usize;
+    for i in 0..grid.h {
+        for j in 0..grid.w {
+            let scene_up = i - up[(i, j)];
+            let scene_down = down[(i, j)] - i;
+            let scene_left = j - left[(i, j)];
+            let scene_right = right[(i, j)] - j;
+
+            let scenic_score = scene_up * scene_down * scene_left * scene_right;
+
+            if scenic_score > max {
+                max = scenic_score;
+            }
+        }
+    }
+
+    max
+}
+
+fn compute_visible(grid: &Grid<u8>) -> usize {
+    let w = grid.w;
+    let h = grid.h;
+
+    let mut visible: Grid<bool> = Grid::new(grid.w, grid.h);
 
     // edges
-    for i in 0..h {
-        visible[i * w] = true;
-        visible[i * w + w - 1] = true;
+    for i in 0..grid.h {
+        visible[(i, 0)] = true;
+        visible[(i, w - 1)] = true;
     }
     for j in 0..w {
-        visible[j] = true;
-        visible[(h - 1) * w + j] = true;
+        visible[(0, j)] = true;
+        visible[(h - 1, j)] = true;
     }
 
     // along rows
     for i in 1..h - 1 {
         let mut max = 0;
 
-        let range = i * w..i * w + w;
-
-        for index in range.clone() {
-            if grid[index] > max {
-                visible[index] = true;
-                max = grid[index];
+        for j in 0..w {
+            if grid[(i, j)] > max {
+                visible[(i, j)] = true;
+                max = grid[(i, j)];
             }
         }
         max = 0;
-        for index in range.rev() {
-            if grid[index] > max {
-                visible[index] = true;
-                max = grid[index];
+        for j in (0..w).rev() {
+            if grid[(i, j)] > max {
+                visible[(i, j)] = true;
+                max = grid[(i, j)];
             }
         }
     }
@@ -58,33 +175,21 @@ fn compute_visible(grid: &Vec<u8>, w: usize, h: usize) -> usize {
     for j in 1..w - 1 {
         let mut max = 0u8;
 
-        let range = j..=(h - 1) * w + j;
-
-        for index in range.clone().step_by(w) {
-            if grid[index] > max {
-                visible[index] = true;
-                max = grid[index];
+        for i in 0..h {
+            if grid[(i, j)] > max {
+                visible[(i, j)] = true;
+                max = grid[(i, j)];
             }
         }
 
         max = 0;
-        for index in range.rev().step_by(w) {
-            if grid[index] > max {
-                visible[index] = true;
-                max = grid[index];
+        for i in (0..h).rev() {
+            if grid[(i, j)] > max {
+                visible[(i, j)] = true;
+                max = grid[(i, j)];
             }
         }
     }
 
-    visible.iter().filter(|b| **b).count()
-}
-
-fn display_visible(visible: &Vec<bool>, w: usize, h: usize) {
-    for i in 0..h {
-        for j in 0..w {
-            let symbol = if visible[i * w + j] { "*" } else { "." };
-            print!("{symbol} ");
-        }
-        println!();
-    }
+    visible.v.iter().filter(|b| **b).count()
 }
