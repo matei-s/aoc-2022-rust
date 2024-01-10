80a75b070f35bc0acd97f5623b8c9777f19ab0f1
Matei Simtinică <matei.simtinica@gmail.com>
2024-01-10 17:46:57 +0200

    day 14, part 2

--- day14_regolith_reservoir/src/main.rs
+++ day14_regolith_reservoir/src/main.rs
@@ -1,8 +1,6 @@
 use std::{
     cmp::{max, min},
     io::stdin,
-    thread::sleep,
-    time::Duration,
 };
 
 use cell::Cell;
@@ -14,29 +12,29 @@ pub mod grid;
 type Trace = Vec<(usize, usize)>;
 type Cave = Grid<u8>;
 
+const BG: &str = "\x1b[90m░░\x1b[0m";
+const ROCK: &str = "\x1b[37m██\x1b[0m";
+const SAND: &str = "\x1b[33m▒▒\x1b[0m";
+
 fn main() {
     let lines: Vec<String> = stdin().lines().filter_map(Result::ok).collect();
-    let (traces, min_x, max_x, max_y) = parse_traces(&lines);
+    let (traces, max_y) = parse_traces(&lines);
 
-    let (mut cave, spawn_cell) = make_cave(&traces, min_x, max_x, max_y);
+    let (mut cave, spawn_cell) = make_cave(&traces, max_y);
 
-    let total_sand = fill_with_sand(&mut cave, spawn_cell, false);
+    let (sand_to_floor, total_sand) = fill_with_sand(&mut cave, spawn_cell);
 
-    println!("part 1: {total_sand}");
+    println!("part 1: {sand_to_floor}");
+    println!("part 2: {total_sand}");
 }
 
-fn fill_with_sand(cave: &mut Cave, spawn_cell: Cell, animate: bool) -> u32 {
+fn fill_with_sand(cave: &mut Cave, spawn_cell: Cell) -> (u32, u32) {
+    let mut total_sand = 0;
+
     'outer: loop {
         let mut sand = spawn_cell;
-
         loop {
-            if animate {
-                clearscreen::clear().expect("Failed to clear the screen");
-                print_cave(&cave, sand);
-                sleep(Duration::from_millis(20));
-            }
-
-            if sand.i as usize >= cave.h - 1 {
+            if sand.i as usize >= cave.h - 2 {
                 break 'outer;
             } else if cave[sand.down()] == 0 {
                 sand = sand.down();
@@ -46,32 +44,45 @@ fn fill_with_sand(cave: &mut Cave, spawn_cell: Cell, animate: bool) -> u32 {
                 sand = sand.dr();
             } else {
                 cave[sand] = 2;
+                total_sand += 1;
                 break;
             }
         }
     }
 
-    let mut total_sand = 0;
-    for i in 0..cave.h {
-        for j in 0..cave.w {
-            if cave[(i, j)] == 2 {
-                total_sand += 1
+    let sand_to_floor = total_sand;
+
+    print_cave(&cave);
+
+    loop {
+        let mut sand = spawn_cell;
+        if cave[sand] == 2 {
+            break;
+        }
+        loop {
+            if cave[sand.down()] == 0 {
+                sand = sand.down();
+            } else if cave[sand.dl()] == 0 {
+                sand = sand.dl();
+            } else if cave[sand.dr()] == 0 {
+                sand = sand.dr();
+            } else {
+                cave[sand] = 2;
+                total_sand += 1;
+                break;
             }
         }
     }
 
-    if !animate {
-        print_cave(cave, spawn_cell);
-    }
+    println!();
+    print_cave(&cave);
 
-    total_sand
+    (sand_to_floor, total_sand)
 }
 
-fn parse_traces(lines: &Vec<String>) -> (Vec<Vec<(usize, usize)>>, usize, usize, usize) {
+fn parse_traces(lines: &Vec<String>) -> (Vec<Vec<(usize, usize)>>, usize) {
     let mut traces: Vec<Vec<(usize, usize)>> = Vec::new();
 
-    let mut min_x = usize::MAX;
-    let mut max_x = usize::MIN;
     let mut max_y = usize::MIN;
 
     for line in lines {
@@ -80,19 +91,17 @@ fn parse_traces(lines: &Vec<String>) -> (Vec<Vec<(usize, usize)>>, usize, usize,
             let tokens: Vec<&str> = xy.split(",").collect();
             let x = tokens[0].parse().unwrap();
             let y = tokens[1].parse().unwrap();
-            min_x = min_x.min(x);
-            max_x = max_x.max(x);
             max_y = max_y.max(y);
             trace.push((x, y));
         }
         traces.push(trace);
     }
 
-    (traces, min_x, max_x, max_y)
+    (traces, max_y)
 }
 
-fn make_cave(traces: &Vec<Trace>, min_x: usize, max_x: usize, max_y: usize) -> (Cave, Cell) {
-    let mut cave = Grid::<u8>::new(max_x - min_x + 3, max_y + 2);
+fn make_cave(traces: &Vec<Trace>, max_y: usize) -> (Cave, Cell) {
+    let mut cave = Grid::<u8>::new(2 * max_y + 5, max_y + 3);
 
     for trace in traces {
         for slice in trace.windows(2) {
@@ -101,36 +110,35 @@ fn make_cave(traces: &Vec<Trace>, min_x: usize, max_x: usize, max_y: usize) -> (
 
             for i in min(y1, y2)..=max(y1, y2) {
                 for j in min(x1, x2)..=max(x1, x2) {
-                    let j = j - min_x + 1;
+                    let j = j + cave.w / 2 - 500;
                     cave[(i, j)] = 1;
                 }
             }
         }
     }
 
+    let floor_i = cave.h - 1;
+    for j in 0..cave.w {
+        cave[(floor_i, j)] = 1;
+    }
+
     let spawn_cell = Cell {
         i: 0,
-        j: 500 - min_x as isize + 1,
+        j: cave.w as isize / 2,
     };
+
+    cave[spawn_cell] = 3;
 
     (cave, spawn_cell)
 }
 
-fn print_cave(cave: &Cave, sand_cell: Cell) {
+fn print_cave(cave: &Cave) {
     for i in 0..cave.h {
         for j in 0..cave.w {
-            if (Cell {
-                i: i as isize,
-                j: j as isize,
-            }) == sand_cell
-            {
-                print!("\x1b[33m▓▓\x1b[0m");
-                continue;
-            }
             match cave[(i, j)] {
-                0 => print!("\x1b[90m░░\x1b[0m"),
-                1 => print!("\x1b[37m██\x1b[0m"),
-                2 | 3 => print!("\x1b[33m▒▒\x1b[0m"),
+                0 => print!("{BG}"),
+                1 => print!("{ROCK}"),
+                2 | 3 => print!("{SAND}"),
                 _ => panic!("unrecognized cave cell"),
             }
         }
